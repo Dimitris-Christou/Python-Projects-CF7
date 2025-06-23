a72a7d4e64f4d3b8c62227622d94e2ff9b39d893
Dimitris Christou <dchristou24@gmail.com>
2025-06-23 18:16:45 +0300

    added comment

--- chapter01/05_input_demo.py
+++ chapter01/05_input_demo.py
@@ -6,7 +6,7 @@ year_of_birth = int(input("Please insert the year of your birth: "))
 
 print("You are", 2025 - year_of_birth, "years old")
 
-print("You are", 2025 - int(input("Please insert the year of your birth: ")), "years old")
+print("You are", 2025 - int(input("Please insert the year of your birth: ")), "years old")  #Η print() function επιστρέφει σε μορφή string!
 
 
 
