4622b4a74cbcc4836195a0c2bbc34378c3d298ae
Dimitris Christou <dchristou24@gmail.com>
2025-06-25 18:48:21 +0300

    added delete, pop function in module 09 of chapter02

--- chapter02/09_dictionaries_op.py
+++ chapter02/09_dictionaries_op.py
@@ -28,3 +28,41 @@ print(product_1)
 
 product_10 = products.get(10, "Product not found")
 print(product_10)
+
+# delete function in dicts
+del products[1]
+print(f"After deleting key 1 : {products}")
+
+# pop function in dicts
+removed_item = products.pop(3, "Item not found")
+print(f"Removed item: {removed_item}")
+print(f"After popping: {products}")
+
+# Delete: remove the 'last' in serted item with popitem
+key, value = products.popitem()
+print(f"key: {key}, value: {value}")
+print(products)
+
+key_to_check = 2
+
+if key_to_check in products:
+    print(f"Key: {key} exists")
+else:
+    print(f"key: {key_to_check} does not exist")
+
+# iterate
+
+for key in products:
+    print(key, ":", products[key])
+
+print("-"*10)
+for key in products.keys():
+    print(key, ":", products[key])
+
+print("-"*10)
+for value in products.values():
+    print(value)
+
+print("-"*10)
+for key, value in products.items():
+    print(f"{key}, {value}")
