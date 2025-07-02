46f73ff00a8698359b1d6ace7513dd9f32c1ac4a
Dimitris Christou <dchristou24@gmail.com>
2025-07-02 19:34:28 +0300

    finished module 12

--- chapter04/12_fibo_rec.py
+++ chapter04/12_fibo_rec.py
@@ -2,4 +2,12 @@ def fibo(n:int) -> int:
     #fibo(n) = fibo(n - 1) = fibo(n -2)
     # fibo(0) = 0
     # fibo(1) = 1
-    pass
+    if n in (0,1):return n
+    return finbo(n - 1) + fibo(n - 2)
+
+def main():
+    n = int(input("Please given an integer: "))
+    print(f"fibo({n}) = {fibo(n)}")
+
+if __name__ == "__main__":
+    main()
